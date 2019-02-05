f202ba6294c5f5b67e7b62c66705e30251697bd4
christopher <tomiwa_chris@yahoo.com>
2019-02-05 17:23:34 +0100

    implementing states and props on noughts and cross game

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "./components/Square";
+import Board from "./components/Board";
 
-const name = "Chris";
+const name = "world";
 const element = <h1>hello, {name}</h1>;
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<Board />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
